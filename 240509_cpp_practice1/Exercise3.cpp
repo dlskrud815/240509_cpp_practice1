b96f6a8102161a9b49c11334f97c6e0e01cd5373
dlskrud815 <dlskrud815@gmail.com>
2024-05-09 19:11:10 +0900

    주석 추가

--- 240509_cpp_practice1/Exercise3.cpp
+++ 240509_cpp_practice1/Exercise3.cpp
@@ -6,7 +6,9 @@
 
 int main()
 {
+	//고객단에서 사용 가능한 서비스와 그에 필요한 필드 정보
 	CustomerService customerService;
+	//은행단에서 사용 가능한 서비스와 그에 따라 접근 가능한 필드 정보
 	BankService bankService;
 
 	customerService.CheckAccount();
