6bdcbdfacf2033a501b9122e397f5bd2f16ae86a
dlskrud815 <dlskrud815@gmail.com>
2024-05-09 19:17:09 +0900

    customerId to v_customerId

--- 240509_cpp_practice1/BankService.h
+++ 240509_cpp_practice1/BankService.h
@@ -9,7 +9,7 @@ class BankService
 private:
 	string branchId, address;
 	int customerNum=0, activeAccountNum=0;
-	vector <string> customerId; //해당 지점에서 관리 중인 고객 아이디 목록
+	vector <string> v_customerId; //해당 지점에서 관리 중인 고객 아이디 목록
 
 public:
 	void OpenNewAccount();
