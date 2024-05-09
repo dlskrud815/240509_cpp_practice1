93135e671fc3c2fc69a7d7f73aca99d74a3cda34
dlskrud815 <dlskrud815@gmail.com>
2024-05-09 16:03:05 +0900

    Exercise2 구조체 실습

---
+++ 240509_cpp_practice1/Exercise2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+struct Rectangle
+{
+	float width = 0;
+	float height = 0;
+};
+
+int main()
+{
+	Rectangle rec;
+
+	cout << "가로, 세로 길이를 입력하세요: ";
+	cin >> rec.width >> rec.height;
+
+	cout << "넓이는.. " << rec.width * rec.height << endl;
+}
